020e6d16e5683a888aaace083a20f16cf1445997
Mic Szillat <mic@team.uber.space>
2017-04-04 10:24:51 +0200

    :sparkles: observe pipeline build times

--- gitlab_webhook_handler.py
+++ gitlab_webhook_handler.py
@@ -1,7 +1,20 @@
-import json
-from StringIO import StringIO
+from prometheus_client import Summary
+
+
+pipeline_duration_seconds = Summary(
+    'gitlab_pipeline_duration_seconds',
+    'Time the pipeline took to run',
+    ['project_name'])
+
+
+def gitlab_to_prometheus(data):
+    duration = data["object_attributes"]["duration"]
+    project_name = data["project"]["name"]
+    label = pipeline_duration_seconds.labels(project_name=project_name)
+    label.observe(duration)
 
 
 def process_gitlab_data(request):
     data = request.json
-    return json.dumps(data)
+    gitlab_to_prometheus(data)
+    return "Thank you"
