e287cc1efbbd1862d6a440dda3005ef08454bd61
Mic Szillat <mic@team.uber.space>
2017-04-04 16:56:20 +0200

    :lock: require a secret for GitLab hooks

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from bottle import route, run, request
+import os
+from bottle import route, run, request, abort
 from prometheus_metrics import get_prometheus_metrics
 from gitlab_webhook_handler import process_gitlab_data
+
+
+def check_secret(request):
+    token = request.headers.get('X-Gitlab-Token')
+    if token == secret:
+        return True
+    else:
+        abort(401, "Access denied.")
 
 
 @route('/metrics', 'GET')
@@ -10,7 +19,12 @@ def metrics():
 
 @route('/gitlab', 'POST')
 def gitlab():
+    check_secret(request)
     return process_gitlab_data(request)
 
 
-run(host='::', port=8080)
+host = os.environ.get('HOST', '::')
+port = os.environ.get('PORT', 8080)
+secret = os.environ.get('SECRET', '')
+
+run(host=host, port=port)
